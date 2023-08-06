5b9a2a296271d39e18b531a863b56955a253d506
Joakim Wirén <joakim@wiren.cc>
2023-08-06 23:33:48 +0200

    Configs for prettier and eslint

---
+++ .prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: false,
+  semi: true,
+  trailingComma: "es5",
+  printWidth: 100,
+  endOfLine: "auto",
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindConfig: "./tailwind.config.ts",
+};
