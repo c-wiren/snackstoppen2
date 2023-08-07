b4fb1ce93ee93d62c34e0dcc195423ebb4442366
Joakim Wirén <joakim@wiren.cc>
2023-08-07 21:51:34 +0200

    Updated configs + tailwind-prettier install
    Still some eslint-prettier problems

--- .prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  singleQuote: false,
-  semi: true,
-  trailingComma: "es5",
-  printWidth: 100,
-  endOfLine: "auto",
-  plugins: ["prettier-plugin-tailwindcss"],
-  tailwindConfig: "./tailwind.config.ts",
-};
