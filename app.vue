38bab87924ee65a060d5d916ac9f7a58b2decdf8
Joakim Wirén <47276887+j-wiren@users.noreply.github.com>
2023-08-20 08:15:12 +0200

    💩 Base styling (#3)

--- app.vue
+++ app.vue
@@ -1,12 +1,17 @@
 <template>
   <NuxtLayout>
-    <header>
-      <RouterLink to="/">Snackstoppen</RouterLink>
+    <header class="fixed flex w-screen justify-center gap-4 bg-white p-4 font-bold">
+      <RouterLink to="/" class="mr-4 uppercase">Snackstoppen</RouterLink>
       <RouterLink to="/">Hem</RouterLink>
       <RouterLink to="/snacks">Snacks</RouterLink>
       <RouterLink to="/toppen">Toppen</RouterLink>
       <RouterLink to="/om-sidan">Om sidan</RouterLink>
     </header>
-    <NuxtPage />
+    <NuxtPage class="grid min-h-screen place-content-center" />
   </NuxtLayout>
 </template>
+<style scoped lang="pcss">
+a {
+  @apply text-black no-underline;
+}
+</style>
