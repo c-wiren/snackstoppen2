65e9fcb2a485a88acb8029c2a7021ba90509ceb3
j-wiren <joakim@wiren.cc>
2025-11-02 16:43:30 +0100

    ⬆️ Upgrade Nuxt to v4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: "2025-11-02",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   postcss: {
